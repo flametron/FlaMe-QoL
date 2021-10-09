b699bd0a9b1ccc3c673e0d88f2b69b355b6a6b9e
Sayan Kundu <sayanisflame@gmail.com>
2021-10-10 03:47:35 +0530

    Jaak, finally add korlam

--- src/lifereallysux.js
+++ src/lifereallysux.js
@@ -20,6 +20,10 @@ class fuckIt
         const database = DatabaseServer.tables;
         const globals = database.globals.config;
         const items = database.templates.items;
+
+		
+
+
 		if(config.useMasterForWeightLimit){
 			globals.Stamina.WalkOverweightLimits.x = config.masterWeightLimit;
 			globals.Stamina.WalkOverweightLimits.y = config.masterWeightLimit;
@@ -72,6 +76,30 @@ class fuckIt
 				globals.EventType = ["Halloween"];
 			}	
 		}
+		// add my favourite sights
+		setTimeout(this.addSights,2000);
+	}
+
+	addSights(){
+		
+        const items = DatabaseServer.tables.templates.items;
+		const frontsights = ["5d3eb536a4b9363b1f22f8e2","5c07b36c0db834002a1259e9","5f3e78a7fbf956000b716b8e","5a7d90eb159bd400165484f1","5e8708d4ae379e67d22e0102","5a7d9104159bd400134c8c21"];
+		const rearsights = ["5c07b3850db834002330045b","5d3eb4aba4b93650d64e497d","5f3e7897ddc4f03b010e204a","5a7d9122159bd4001438dbf4","5a7d912f159bd400165484f3"];
+		for (let i in items){
+			if (items[i]._parent === "55818a104bdc2db9688b4569")
+				for(let j in items[i]._props.Slots)
+					if(items[i]._props.Slots[j]._name==="mod_sight_front"){
+						for(let sght in frontsights)
+							items[i]._props.Slots[j]._props.filters[0].Filter.push(frontsights[sght]);
+					}
+			if (items[i]._parent === "55818a304bdc2db5418b457d")
+				for(let j in items[i]._props.Slots)
+					if(items[i]._props.Slots[j]._name==="mod_sight_rear"){
+						for(let sght in rearsights)
+							items[i]._props.Slots[j]._props.filters[0].Filter.push(rearsights[sght]);
+					}
+		}
+		Logger.info(`CUSTOM SIGHTS ADDED`);
 	}
 }
 
